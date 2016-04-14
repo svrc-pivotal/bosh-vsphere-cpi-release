78ea2296014e2099a986b3767ed45a209366cbfb
Corey Innis <cinnis@pivotal.io>
2016-04-14 16:22:41 -0700

    Adds unit test tasks.
    Signed-off-by: Edwin Xie <exie@pivotal.io>

---
+++ ci/tasks/run-unit.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+release_dir="$( cd $(dirname $0) && cd ../.. && pwd )"
+workspace_dir="$( cd ${release_dir} && cd .. && pwd )"
+
+source ${release_dir}/ci/tasks/utils.sh
+source /etc/profile.d/chruby.sh
+chruby 2.1.2
+
+pushd "${release_dir}"
+  echo "running unit tests"
+  pushd src/vsphere_cpi
+    bundle install
+    bundle exec rspec spec/unit
+  popd
+popd
